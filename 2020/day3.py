31b8a9d88c81e461490068bf552d18ef7343a89e
Tulki <127dhawkins@gmail.com>
2020-12-02 21:39:05 -0800

    day3

---
+++ 2020/day3.py
@@ -0,0 +1,40 @@
+from shared import *
+
+
+class SnowSlope:
+    def __init__(self, slopeMap):
+        self.map = slopeMap
+        self.width = len(slopeMap[0])
+        self.length = len(slopeMap)
+
+    def is_tree(self, x, y):
+        mapX = x % self.width
+        mapY = y
+
+# Part A and B
+def solveA():
+    slopeMap = split_lines('day3.input')
+    result = recurse(slopeMap, 0, 3, 1)
+    print(result)
+
+def solveB():
+    slopeMap = split_lines('day3.input')
+    result1 = recurse(slopeMap, 0, 1, 1)
+    result2 = recurse(slopeMap, 0, 3, 1)
+    result3 = recurse(slopeMap, 0, 5, 1)
+    result4 = recurse(slopeMap, 0, 7, 1)
+    result5 = recurse(slopeMap, 0, 1, 2)
+    result = result1*result2*result3*result4*result5
+    print(result)
+
+def recurse(slopeMap, xCurrent, xJump, yJump):
+    if len(slopeMap) == 0:
+        return 0
+    
+    line = slopeMap[0]
+    xNext = (xCurrent + xJump) % len(line)
+
+    if line[xCurrent] == '#':
+        return 1 + recurse(slopeMap[yJump:], xNext, xJump, yJump)
+    else:
+        return recurse(slopeMap[yJump:], xNext, xJump, yJump)
